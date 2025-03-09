a4d51095319a4b6a7ba8ee811aed226ae07f6d3d
shrish <shrishtika.20230907@iit.ac.lk>
2025-03-09 22:53:41 +0530

    changed style again

--- src/pages/Auth/FrogotPassword.tsx
+++ src/pages/Auth/FrogotPassword.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useState } from 'react';
 import { TextField, Button, Typography, Container, Paper, Box } from '@mui/material';
 import { Mail } from 'lucide-react';
 import { Link } from 'react-router-dom';
